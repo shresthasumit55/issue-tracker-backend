28be13040d981d6627b11b2a0cbea47e46da8e6a
pratistha <pratisthashres@gmail.com>
2019-12-03 17:02:29 -0600

    Inserting Dummy Data

--- src/main/resources/insert.sql
+++ src/main/resources/insert.sql
@@ -19,3 +19,20 @@ INSERT INTO `db_issuetracker`.`issue` (`created_date`, `description`, `due_date`
 INSERT INTO `db_issuetracker`.`user` (`email`, `firstname`, `last_name`, `middle_name`) VALUES ('mike@gmail.com', 'mike', 'scott', 'bob');
 INSERT INTO `db_issuetracker`.`project_user` (`project_id`, `user_id`) VALUES ('1', '3');
 INSERT INTO `db_issuetracker`.`user_role_mapping` (`project_id`, `role_id`, `user_id`) VALUES ('1', '1', '3');
+
+
+INSERT INTO project(name, key_id, description) VALUES('Issue Tracking', 1, 'Tracking different issues in a project');
+INSERT INTO project(name, key_id, description) VALUES('Library Management', 2, 'Manage the book lending services');
+INSERT INTO project(name, key_id, description) VALUES('Code Duplication', 3, 'Find out the type of code duplication in a project');
+INSERT INTO project(name, key_id, description) VALUES('Property Management', 4, 'List and sell properties');
+
+INSERT INTO role(name,description) VALUES('Project Manager','Creates and Manages Projects for a team');
+INSERT INTO role(name,description) VALUES('Developer','Solves issues created in a project');
+INSERT INTO role(name,description) VALUES('Tester','Verifies the solution made by the developer');
+INSERT INTO role(name,description) VALUES('Business Analyst','Creates issues in a Project');
+
+INSERT INTO user(email, firstname, lastname) VALUES ('pratistha.shrestha@email.com','Pratistha','Shrestha');
+INSERT INTO user(email, firstname, lastname) VALUES ('sumit.shrestha@email.com','Sumit','Shrestha');
+
+
+
